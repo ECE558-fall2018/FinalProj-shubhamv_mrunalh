4469cd0ff9ee94e4ea1bd5a2e8ac909ae0203a61
mrunal <mrunalhirve@gmail.com>
2018-12-05 22:14:20 -0800

    Setting switch for Robot On/Off and FaceDetection

--- PerceptoBot/app/src/main/java/com/example/mrunal/perceptobot/MainActivity.java
+++ PerceptoBot/app/src/main/java/com/example/mrunal/perceptobot/MainActivity.java
@@ -13,7 +13,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,9 +25,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor mSensor;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
-    private Switch mReverse;
+    private Switch mReverse, mRobotOn, mFaceDetection;
     private WebView mWebview;
-    private String IP ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +35,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         database =  FirebaseDatabase.getInstance();
         myRef = database.getReference();
-
-
 
         mWebview = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = mWebview.getSettings();
@@ -52,11 +48,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
-
-
         mReverse = (Switch) findViewById(R.id.switch1);
+        mRobotOn = (Switch) findViewById(R.id.switch2);
+        mFaceDetection = (Switch) findViewById(R.id.switch3);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        mRobotOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myRef.child("robotOn").setValue(isChecked);
+                if (isChecked){
+                    mFaceDetection.setChecked(false);
+                }
+            }
+        });
+
         mReverse.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -64,10 +71,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
+        mFaceDetection.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myRef.child("faceDetection").setValue(isChecked);
+                if (isChecked){
+                    mRobotOn.setChecked(false);
+                }
+            }
+        });
+
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 mWebview.loadUrl("http://" + dataSnapshot.child("IP").getValue(String.class) + ":8082");
+                mReverse.setChecked((Boolean) dataSnapshot.child("direction").getValue());
+                mFaceDetection.setChecked((Boolean) dataSnapshot.child("faceDetection").getValue());
             }
 
             @Override
@@ -77,7 +96,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
     }
 
-
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
@@ -86,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+        myRef.child("robotOn").setValue(false);
     }
 
     @Override
@@ -94,28 +113,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         x = event.values[0];
         if(x > 0 && x < 9) {
             x = 1-(x/9);
-            //myRef.child("linearAcc").setValue(1-(x/9));
         } else {
             x = 0;
-            //myRef.child("linearAcc").setValue(0);
         }
 
         y = event.values[1];
         if ((y < -1 && y > -6 ) || (y > 1 && y < 6 )) {
             y = y;
             x = 0;
-            //myRef.child("lateralAcc").setValue(y);
-            //myRef.child("linearAcc").setValue(0);
         } else {
             y = 0;
-            //myRef.child("lateralAcc").setValue(0);
         }
 
         myRef.child("linearAcc").setValue(x);
         myRef.child("lateralAcc").setValue(y);
     }
-
-
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
